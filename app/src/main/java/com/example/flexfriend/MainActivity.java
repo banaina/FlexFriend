ac2ec35ea8f2a18902d76deaf616955a1ecb4c39
Arriyan <arriyanalia@gmail.com>
2024-03-07 19:27:05 -0800

    Added intents and files for Progress + Routines Pages

--- app/src/main/java/com/example/flexfriend/MainActivity.java
+++ app/src/main/java/com/example/flexfriend/MainActivity.java
@@ -2,14 +2,38 @@ package com.example.flexfriend;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button camera;
+    private Button intro;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        camera = findViewById(R.id.camActivity);
+        camera.setOnClickListener(this);
+        intro = findViewById(R.id.introActivity);
+        intro.setOnClickListener(this);
     }
 
-    //hello
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.camActivity){
+            Intent intent = new Intent(this, ProgressActivity.class);
+            Toast.makeText(this, "heading to progress viewing activity",Toast.LENGTH_SHORT).show();
+            startActivity(intent);
+        }
+        if (v.getId() == R.id.introActivity){
+            Intent intent = new Intent(this, RoutinesActivity.class);
+            Toast.makeText(this, "heading to routine creation activity",Toast.LENGTH_SHORT).show();
+            startActivity(intent);
+        }
+    }
 }
---
+++ app/src/main/java/com/example/flexfriend/RoutinesActivity.java
@@ -0,0 +1,14 @@
+package com.example.flexfriend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RoutinesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_routines);
+    }
+}
