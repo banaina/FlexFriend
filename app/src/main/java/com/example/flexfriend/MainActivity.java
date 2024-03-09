48a5e7be50b7f8e57aad9335468d1a05bce36759
Arriyan <arriyanalia@gmail.com>
2024-03-08 20:56:08 -0800

    testing

--- app/src/main/java/com/example/flexfriend/MainActivity.java
+++ app/src/main/java/com/example/flexfriend/MainActivity.java
@@ -10,16 +10,13 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button camera;
-    private Button intro;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        camera = findViewById(R.id.camActivity);
+        Button camera = findViewById(R.id.camActivity);
         camera.setOnClickListener(this);
-        intro = findViewById(R.id.introActivity);
+        Button intro = findViewById(R.id.introActivity);
         intro.setOnClickListener(this);
     }
 
@@ -36,4 +33,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             startActivity(intent);
         }
     }
+
+    //testing
 }
