18a3828f4561d6a1adb8b3aa83ad768499a98f88
Arriyan <arriyanalia@gmail.com>
2024-03-14 08:03:49 -0700

    added camera access on progress page

--- app/src/main/java/com/example/flexfriend/ProgressActivity.java
+++ app/src/main/java/com/example/flexfriend/ProgressActivity.java
@@ -1,14 +1,86 @@
 package com.example.flexfriend;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class ProgressActivity extends AppCompatActivity {
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.concurrent.ExecutionException;
+
+public class ProgressActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button cameraButton, galleryButton;
+    private ImageCapture imageCapture;
+    private static final int img_id = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_camera);
+        setContentView(R.layout.activity_progress);
+
+        cameraButton = findViewById(R.id.cameraButton);
+        cameraButton.setOnClickListener(this);
+        galleryButton = findViewById(R.id.galleryButton);
+        galleryButton.setOnClickListener(this);
+    }
+
+    //get an instance of ProcessCameraProvider
+    private void startCamera() {
+        final ListenableFuture<ProcessCameraProvider> cameraProviderFuture
+                = ProcessCameraProvider.getInstance(this);
+        cameraProviderFuture.addListener(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                    bindPreview(cameraProvider);
+
+                } catch (ExecutionException | InterruptedException e) {
+                    // This should never be reached.
+                }
+            }
+        }, ContextCompat.getMainExecutor(this));
+    }
+
+    //selecting a camera and binding to the lifecycle
+    void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
+
+        cameraProvider.unbindAll();
+
+        Preview preview = new Preview.Builder()
+                .build();
+
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
+
+
+        imageCapture = new ImageCapture.Builder()
+                .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+                .build();
+//        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+        cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.cameraButton){
+            Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(camera_intent, img_id);
+        }
+        if (v.getId() == R.id.galleryButton){
+            //send to gallery
+        }
     }
 }
